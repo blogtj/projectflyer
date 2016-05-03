2516ec66cab83f8c88f6ae698aff9b9cf1f27529
Adrian <tudoroiuj@yahoo.com>
2016-05-03 10:34:27 +0300

    install laravel

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,7 +38,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Laravel 5 - blogtj</div>
+                <div class="title">Laravel 5 - blogtj 2</div>
             </div>
         </div>
     </body>
